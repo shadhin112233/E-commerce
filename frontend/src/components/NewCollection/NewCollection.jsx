1604000c4cd14caaafcedce63d5beab96fed1ada
Shadhin112233 <sharoareshadhin271@gmail.com>
2026-06-03 11:35:27 +0600

    Fix localhost URL and state mapping in Popular component

--- frontend/src/components/NewCollection/NewCollection.jsx
+++ frontend/src/components/NewCollection/NewCollection.jsx
@@ -1,36 +1,38 @@
 import React from 'react'
 import './NewCollection.css'
-import { useState,useEffect } from 'react'
-
+import { useState, useEffect } from 'react'
 import Item from '../Item/Item'
 
 const NewCollection = () => {
-
   const [new_collection, setNew_collection] = useState([]);
+  const backend_url = "https://e-commerce-backendd-zm8m.onrender.com";
 
   useEffect(() => {
-    fetch('http://localhost:4000/newcollection')
-    .then((response)=>response.json())
-    .then((data)=>setNew_collection(data))
-  
-    
+    // লোকালহোস্ট বদলে লাইভ URL দেওয়া হলো
+    fetch(`${backend_url}/newcollection`)
+      .then((response) => response.json())
+      .then((data) => setNew_collection(data))
   }, [])
-  
-
 
   return (
     <div className='newcollections'>
         <h1>NEW COLLECTIONS</h1>
         <hr/>
         <div className="collections">
-            {new_collection.map((item,i)=>{
-                 return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+            {new_collection.map((item, i) => {
+                // ডাটাবেজের ছবিতে localhost:4000 থাকলে তা লাইভ URL দিয়ে পরিবর্তন করার ট্রিক
+                const fixedImage = item.image.replace('http://localhost:4000', backend_url);
+                
+                return <Item 
+                  key={i} 
+                  id={item.id} 
+                  name={item.name} 
+                  image={fixedImage} 
+                  new_price={item.new_price} 
+                  old_price={item.old_price}
+                />
             })}
-
         </div>
-
-        
-      
     </div>
   )
 }
--- frontend/src/components/Popular/Popular.jsx
+++ frontend/src/components/Popular/Popular.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import './Popular.css'
-import data_product from "../Asset/data"
 import Item from '../Item/Item'
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 const Popular = () => {
-
-  const [popularProduct, setPopularProduct] = useState({});
+  // ম্যাপ করার সুবিধার্থে ডিফল্ট ভ্যালু অবজেক্ট থেকে অ্যারে ([]) করা হলো
+  const [popularProduct, setPopularProduct] = useState([]);
+  const backend_url = "https://e-commerce-backendd-zm8m.onrender.com";
 
   useEffect(() => {
-    fetch('http://localhost:4000/popularinwomen')
-    .then((response)=>response.json())
-    .then((data)=>setPopularProduct(data))
-    
+    // লোকালহোস্ট বদলে লাইভ URL দেওয়া হলো
+    fetch(`${backend_url}/popularinwomen`)
+      .then((response) => response.json())
+      .then((data) => setPopularProduct(data))
   }, [])
   
   return (
@@ -20,12 +20,20 @@ const Popular = () => {
         <h1>POPULAR IN WOMEN</h1>
         <hr/>
         <div className="popular-item">
-            {data_product.map((item,i)=>{
-                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+            {/* ডেটাবেজের ছবি ফিক্স করে লাইভ popularProduct স্টেটটি ম্যাপ করা হলো */}
+            {popularProduct.map((item, i) => {
+                const fixedImage = item.image.replace('http://localhost:4000', backend_url);
+                
+                return <Item 
+                  key={i} 
+                  id={item.id} 
+                  name={item.name} 
+                  image={fixedImage} 
+                  new_price={item.new_price} 
+                  old_price={item.old_price}
+                />
             })}
         </div>
-
-      
     </div>
   )
 }

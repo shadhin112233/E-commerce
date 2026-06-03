e45c76ee099b9d25f4029648336469518405d4e9
Shadhin112233 <sharoareshadhin271@gmail.com>
2026-06-03 11:47:26 +0600

    Fix image URLs in Item component and live endpoints in LoginSignup

--- frontend/src/components/Item/Item.jsx
+++ frontend/src/components/Item/Item.jsx
@@ -2,24 +2,27 @@ import React from 'react'
 import './Item.css'
 import { Link } from 'react-router-dom'
 
-
 const Item = (props) => {
+  const backend_url = "https://e-commerce-backendd-zm8m.onrender.com";
+
+  // ডাটাবেজের ছবিতে localhost:4000 থাকলে তা লাইভ URL দিয়ে পরিবর্তন করার লজিক
+  const fixedImage = props.image ? props.image.replace('http://localhost:4000', backend_url) : '';
+
   return (
     <div className='item'>
-       <Link to={`/product/${props.id}`}> <img onClick={window.scrollTo(0,0)} src={props.image} alt="" /></Link>
+       <Link to={`/product/${props.id}`}>
+          {/* onClick হ্যান্ডলারটি ফিক্স করা হয়েছে */}
+          <img onClick={() => window.scrollTo(0,0)} src={fixedImage} alt={props.name} />
+       </Link>
         <p>{props.name}</p>
         <div className="item-prices">
             <div className="item-price-new">
-
                 ${props.new_price}
-
             </div>
             <div className="item-price-old">
                  ${props.old_price}
-
             </div>
         </div>
-      
     </div>
   )
 }
--- frontend/src/pages/LoginSignup.jsx
+++ frontend/src/pages/LoginSignup.jsx
@@ -3,9 +3,8 @@ import './CSS/LoginSignup.css'
 import { useState } from 'react'
 
 const LoginSignup = () => {
-
-
   const [state, setState] = useState("Login");
+  const backend_url = "https://e-commerce-backendd-zm8m.onrender.com";
 
   const [formData, setFormData] = useState({
     username:"",
@@ -15,42 +14,21 @@ const LoginSignup = () => {
 
   const changeHandler = (e) =>{
     setFormData({...formData,[e.target.name]:e.target.value})
-
   }
 
   const login = async() =>{
-    console.log("Login Fuction Exucated",formData)
-let responseData;
-    await fetch('http://localhost:4000/login',{
-      method:'POST',
-      headers:{
-        Accept:'application/form-data',
-        'Content-Type':'application/json',
-
-      },
-      body:JSON.stringify(formData),
-    }).then((response)=> response.json()).then((data) =>responseData=data)
-
-    if(responseData.success){
-      localStorage.setItem('auth-token',responseData.token)
-      window.location.replace("/")
-    }else{
-      alert(responseData.errors)
-    }
-
-  }
-  const signup = async() =>{
-    console.log("Signup Fuction Exucated",formData);
+    console.log("Login Function Executed", formData)
     let responseData;
-    await fetch('http://localhost:4000/signup',{
+    
+    // লোকালহোস্ট বদলে লাইভ URL দেওয়া হলো
+    await fetch(`${backend_url}/login`,{
       method:'POST',
       headers:{
-        Accept:'application/form-data',
+        Accept:'application/json', // স্ট্যান্ডার্ড JSON রিকোয়েস্ট অ্যাকসেপ্ট হেডারের জন্য
         'Content-Type':'application/json',
-
       },
       body:JSON.stringify(formData),
-    }).then((response)=> response.json()).then((data) =>responseData=data)
+    }).then((response)=> response.json()).then((data) => responseData=data)
 
     if(responseData.success){
       localStorage.setItem('auth-token',responseData.token)
@@ -58,34 +36,48 @@ let responseData;
     }else{
       alert(responseData.errors)
     }
-
-
   }
 
+  const signup = async() =>{
+    console.log("Signup Function Executed", formData);
+    let responseData;
+    
+    // লোকালহোস্ট বদলে লাইভ URL দেওয়া হলো
+    await fetch(`${backend_url}/signup`,{
+      method:'POST',
+      headers:{
+        Accept:'application/json',
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify(formData),
+    }).then((response)=> response.json()).then((data) => responseData=data)
 
-
+    if(responseData.success){
+      localStorage.setItem('auth-token',responseData.token)
+      window.location.replace("/")
+    }else{
+      alert(responseData.errors)
+    }
+  }
 
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
         <h1>{state}</h1>
         <div className="loginsignup-fields">
-          {state==="Sign Up"?<input name='username'value={formData.username} onChange={changeHandler} type="text"  placeholder='Your Name'/>:<></>}
-          <input name='email' value={formData.email} onChange={changeHandler} type="email"  placeholder='Email Address'/>
-          <input name='password' value={formData.password} onChange={changeHandler} type="password"  placeholder='Password'/>
-
+          {state==="Sign Up"?<input name='username' value={formData.username} onChange={changeHandler} type="text" placeholder='Your Name'/>:<></>}
+          <input name='email' value={formData.email} onChange={changeHandler} type="email" placeholder='Email Address'/>
+          <input name='password' value={formData.password} onChange={changeHandler} type="password" placeholder='Password'/>
         </div>
         <button onClick={()=>{state==="Login"?login():signup()}}>Continue</button>
 
         {state==="Sign Up"?<p className="loginsignup-login">Already have an Account? <span onClick={()=>{setState("Login")}}>Login here</span></p>:<p className="loginsignup-login">Create an Account? <span onClick={()=>{setState("Sign Up")}}>Click Here</span></p>}
         
-        
         <div className="loginsignup-agree">
           <input type="checkbox" name ='' id=''/>
-          <p>By Continuing,i agree to the terms of use & privecy policy</p>
+          <p>By Continuing, i agree to the terms of use & privacy policy</p>
         </div>
       </div>
-      
     </div>
   )
 }
